6555f951c3447bc4ba04fe9297d7ffe32476ebd9
Ivan Bryzzhin <brizjin@gmail.com>
2017-02-19 16:36:48 +0300

    step2

--- run.bat
+++ run.bat
@@ -4,15 +4,24 @@ SET WD2=c:/ibso/oracle-12c-docker/
 goto %1
 :step0
 docker run --rm -it -v %WD%step0:/tmp/install oracle-12c:step1 /bin/bash /tmp/install/unzip.sh
+
 :step1
 docker build -t oracle-12c:step1 step1
 REM docker rm step1
-ECHO "bash /tmp/install/install"
-ECHO "<enter>"
-ECHO "exit"
+ECHO ENTER COMMAND: bash /tmp/install/install
+ECHO ENTER COMMAND: <enter>
+ECHO ENTER COMMAND: exit
 docker run --shm-size=4g -it --name step1 -v %WD%step0/database:/tmp/install/database oracle-12c:step1 /bin/bash
-
 docker commit step1 oracle-12c:installed
-ECHO "step1 finished"
+ECHO #step1 finished
+
 :step2
-ECHO "step2 started"
+ECHO step2 started
+docker build -t oracle-12c:step2 step2
+ECHO ENTER COMMAND: bash /tmp/create
+docker run --shm-size=4g -ti --name step2 oracle-12c:step2 /bin/bash
+ECHO ENTER COMMAND: exit
+docker commit step2 oracle-12c:created
+
+:step3
+docker build -t oracle-12c step3
