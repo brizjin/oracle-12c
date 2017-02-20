b69668640f298e051431277564a4882e2f23ab11
Brizjin Ivan <brizjin@gmail.com>
2017-02-20 11:57:47 +0300

    step0 в папке ibso

--- ibso.bat
+++ ibso.bat
@@ -3,7 +3,8 @@ SET WD=%WD:\=/%
 SET IBSO_VERSION=ibso_16_5_install
 goto %1
 :step0
-docker run --rm -it -v %WD%step0:/tmp/install oracle-12c:step1 /bin/bash /tmp/install/unzip.sh
+docker run --rm -it -v %WD%ibso/step0:/tmp/install oracle-12c:step1 /bin/bash /tmp/install/unzip.sh
+exit
 
 :step1
 docker build -t ibso:step1 -f %WD%ibso/step1/Dockerfile %WD%ibso/step1
---
+++ ibso/step0/unzip.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd /tmp/install && unzip -n linuxamd64_12102_database_1of2.zip && unzip -n linuxamd64_12102_database_2of2.zip
